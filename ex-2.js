33b79b7bb0bafc7ace611394586b0294027598c3
Napat <napat.wongchr@gmail.com>
2022-03-17 08:52:38 +0700

    add ex1 2 ans

--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,43 @@
-class Notification {}
+class Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
 
-class EmailNotification {}
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class SMSNotification {}
+class EmailNotification extends Notification {
+  constructor(notificationId, createdTime, content, email) {
+    super(notificationId, createdTime, content, email);
+    this.email = email;
+  }
+}
+
+class SMSNotification extends Notification {
+  constructor(notificationId, createdTime, content, phone) {
+    super(notificationId, createdTime, content, phone);
+    this.phone = phone;
+  }
+}
+
+const smsNotification = new SMSNotification(
+  "001",
+  new Date(),
+  "This is sms noti message",
+  "0923232332"
+);
+
+const emailNotification = new EmailNotification(
+  "001",
+  new Date(),
+  "This is email noti message.",
+  "knot@gmail.com"
+);
+
+smsNotification.send();
+emailNotification.send();
